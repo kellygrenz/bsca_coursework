3280bf3b4791255bd539779eb462e17827482483
Kelly Grenz <kellygrenz@Kellys-iMac.local>
2017-10-11 21:04:41 -0600

    pluralizer

---
+++ array_map_intro/app.js
@@ -0,0 +1,13 @@
+const products = ['Apple', 'Avocado', 'Apricot', 'Mango', 'Spicy Ham', 'Feta Cheese', 'Nutella' ]
+
+//console.log(products)
+
+const pluralizer = (productsIn) => {
+    for(i = 0; i < productsIn.length; i++){
+         productsIn[i] += 's'
+    }
+    return productsIn
+}
+
+console.log(pluralizer(products))
+
